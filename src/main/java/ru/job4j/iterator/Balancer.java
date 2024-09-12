26d0f5b9e5b7505f8bdb109c79b223cba3bdc308
slavavoinov <voijnov@yandex.ru>
2024-09-12 16:31:16 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+        while (source.hasNext()) {
+            if (index == nodes.size()) {
+                index = 0;
+            }
+            nodes.get(index++).add(source.next());
+        }
+    }
+}
