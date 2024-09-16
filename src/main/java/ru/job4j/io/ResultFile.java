871ad9e97706e0b867fe87400da2cd6f52c8ca37
slavavoinov <voijnov@yandex.ru>
2024-09-16 20:56:18 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            output.write(System.lineSeparator().getBytes());
+            for (int i = 1; i <= 10; i++) {
+                String tempString = new String("1 * " + i + " = " + i);
+                output.write(tempString.getBytes());
+                output.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
