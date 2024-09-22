a9bd4d14345bfef09f1c6fb499a4b25c42e93abd
slavavoinov <voijnov@yandex.ru>
2024-09-22 11:55:51 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,16 +4,22 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Директория не существует: %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Это не директория: %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         for (File subfile : file.listFiles()) {
-            System.out.printf("Имя файла: %s. Размер файла: %d байт", subfile.getName(), subfile.length());
-            System.out.println(System.lineSeparator());
+            if (subfile.isFile()) {
+                System.out.println(String.format("Name of file: %s, size: %d", subfile.getName(), subfile.length()));
+            } else {
+                System.out.println(String.format("Name of directory: %s", subfile.getName()));
+            }
         }
     }
 }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +10,34 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (parametersValidation(args)) {
+            search(Paths.get(args[0]), path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean parametersValidation(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Incorrect parameters. "
+                    + "The first parameter - folder. "
+                    + "The second - extension.");
+        }
+        File file = new File(args[0]);
+        String extension = args[1];
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!extension.matches("\\.[a-z0-9]{2,5}$")) {
+            throw new IllegalArgumentException(String.format("Incorrect extension: %s", extension));
+        }
+        return true;
     }
 }
