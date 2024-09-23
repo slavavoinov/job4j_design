03280584d6779ce93999221e4fa6df6ea6c3a541
slavavoinov <voijnov@yandex.ru>
2024-09-23 21:06:14 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,11 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -12,15 +15,25 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String string = input.readLine();
-                        System.out.println(string);
-                        if (string != null && string.contains("Bye")) {
-                            server.close();
-                        }
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String string = input.readLine();
+                    System.out.println(string);
+                    String parameter = getParameter(string);
+                    switch (parameter) {
+                        case "Hello" -> output.write("Hello".getBytes());
+                        case "Exit" -> server.close();
+                        default -> output.write("What".getBytes());
+                    }
                     output.flush();
                 }
             }
         }
+    }
+
+    private static String getParameter(String string) {
+        Pattern pattern = Pattern.compile("=.+\s");
+        Matcher matcher = pattern.matcher(string);
+        matcher.find();
+        return matcher.group().trim().substring(1);
     }
 }
